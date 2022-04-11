391e62ef1afbc765ef8b8874d840033a174ba670
ShantelAcker <56419079+shantelacker@users.noreply.github.com>
2022-04-11 07:02:53 -0500

    created a simple way to display the notes list

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
 import "./style.css";
+import { notesList } from "./notesList";
 
 CKEDITOR.replace("editor");
+
+const notes = document.getElementById("notes");
+
+// a function to format each notesList item for display
+function noteFormat(note) {
+  return `
+  <div class="note">
+    <h1>${note.title}</h1>
+    <h2>Category: ${note.category}</h2>
+    <h3>${note.date} ${note.time}</h3>
+    <p>${note.entryText}</p>
+  </div>
+  `;
+}
+
+// map through each element in the notesList array and update the HTML
+notes.innerHTML = notesList.map(noteFormat);
+
+// change the button to say Cancel when clicked
+// and to change back to New Entry when clicked again
---
+++ notesList.js
@@ -0,0 +1,26 @@
+export const notesList = [
+  {
+    id: 1,
+    category: "Kingdom Hearts 1",
+    title: "The Beginning",
+    date: "4/10/22",
+    time: "9:04 PM",
+    entryText: "We started the game, and right now we're in the Tarzan area.",
+  },
+  {
+    id: 2,
+    category: "Kingdom Hearts 1",
+    title: "What we did today",
+    date: "4/10/22",
+    time: "9:04 PM",
+    entryText: "We started the game, and right now we're in the Tarzan area.",
+  },
+  {
+    id: 3,
+    category: "Kingdom Hearts 1",
+    title: "What we did today",
+    date: "4/10/22",
+    time: "9:04 PM",
+    entryText: "We started the game, and right now we're in the Tarzan area.",
+  },
+];
