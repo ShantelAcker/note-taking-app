a8cd55d9e85df5f053778ed1b1003537f8012731
ShantelAcker <56419079+shantelacker@users.noreply.github.com>
2022-04-11 08:57:31 -0500

    added new entry toggle feature

--- main.js
+++ main.js
@@ -1,19 +1,22 @@
 import "./style.css";
 import { notesList } from "./notesList";
 
-CKEDITOR.replace("editor");
-
+// accessing html elements
 const notes = document.getElementById("notes");
+const entryButton = document.getElementById("entry-button");
+const editor = document.getElementById("editor");
+
+// CKEDITOR.replace("editor");
 
 // a function to format each notesList item for display
 function noteFormat(note) {
   return `
-  <div class="note">
-    <h1>${note.title}</h1>
-    <h2>Category: ${note.category}</h2>
-    <h3>${note.date} ${note.time}</h3>
-    <p>${note.entryText}</p>
-  </div>
+    <div class="note">
+      <h1>${note.title}</h1>
+      <h2>Category: ${note.category}</h2>
+      <h3>${note.date} ${note.time}</h3>
+      <p>${note.entryText}</p>
+    </div>
   `;
 }
 
@@ -22,3 +25,14 @@ notes.innerHTML = notesList.map(noteFormat);
 
 // change the button to say Cancel when clicked
 // and to change back to New Entry when clicked again
+entryButton.addEventListener("click", updateEntryButton);
+
+function updateEntryButton() {
+  if (entryButton.innerText === "New Entry") {
+    entryButton.innerText = "Cancel";
+    editor.style.display = "block";
+  } else {
+    entryButton.innerText = "New Entry";
+    editor.style.display = "none";
+  }
+}
