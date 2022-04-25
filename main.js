5dbc4ea2cb3f334f19940ce53f3561ad09faa7a3
ShantelAcker <56419079+shantelacker@users.noreply.github.com>
2022-04-24 20:24:48 -0500

    removed unused import and began adding note entry functionality

--- main.js
+++ main.js
@@ -4,16 +4,14 @@ import { notesList } from "./notesList";
 // accessing html elements
 const notes = document.getElementById("notes");
 const entryButton = document.getElementById("entry-button");
-const editor = document.getElementById("editor");
-
-// CKEDITOR.replace("editor");
+const editorContainer = document.getElementById("editor-container");
+const textEditor = document.getElementById("text-editor");
+const submitButton = document.getElementById("submit-button");
 
 // a function to format each notesList item for display
 function noteFormat(note) {
   return `
     <div class="note">
-      <h1>${note.title}</h1>
-      <h2>Category: ${note.category}</h2>
       <h3>${note.date} ${note.time}</h3>
       <p>${note.entryText}</p>
     </div>
@@ -30,9 +28,15 @@ entryButton.addEventListener("click", updateEntryButton);
 function updateEntryButton() {
   if (entryButton.innerText === "New Entry") {
     entryButton.innerText = "Cancel";
-    editor.style.display = "block";
+    editorContainer.style.display = "block";
   } else {
     entryButton.innerText = "New Entry";
-    editor.style.display = "none";
+    editorContainer.style.display = "none";
   }
+}
+
+submitButton.addEventListener("click", submitEntry);
+
+function submitEntry() {
+  console.log(textEditor.value);
 }
